4c66aa660a4153c1bd6df2364dec603f84da06fc
darshan <darshan.patel.tops@gmail.com>
2023-05-24 16:13:16 +0530

    First Commit

--- class.cpp
+++ class.cpp
@@ -9,13 +9,67 @@ class bank
     string name;
     string type;
     int account;
-    int balance;
+    float balance;
 
-    void givevalue(string name,string type,int account,int balance)
+    void givevalue()
     {
+        cout << "Enter name of depositor: ";
+    getline(cin, name);
+    // cin>>name;
+
+    cout << "Enter account number: ";
+    cin >> account;
+
+    cout << "Enter type of account: ";
+    cin >> type;
+
+    cout << "Enter balance amount: ";
+    cin >> balance;
+
         name = name;
         type = type;
         account = account;
         balance = balance;
     }
+
+    void deposit()
+    {
+        cout << "Enter amount to deposit: ";
+    cin >> balance;
+        balance += balance;
+
+    }
+
+    void withdraw()
+    {
+         cout << "Enter amount to withdraw: ";
+    cin >> balance;
+        if (balance >= balance) 
+    {
+        balance -= balance;
+        cout << "Amount withdrawn successfully.\n";
+    } else 
+    {
+        cout << "Insufficient balance. Withdrawal failed.\n";
+    }
+    }
+    void display()
+    {
+    cout << "Name of the depositor: " << name << endl;
+    cout << "Balance amount in the account: " << balance << endl;
+    }
+
+    bank()
+    {
+        givevalue();
+        deposit();
+        withdraw();
+        display();
+
+    }
 };
+
+int main()
+{
+    bank b;
+}
